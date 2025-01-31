25d87e1b71d0aaee902933b01f804b2b379ff26d
Mohit Sethi <revstudii01@gmail.com>
2025-01-31 19:30:25 +0530

    Update chicken_Drawing.cpp
    Adding Comments to the CODE.

--- chicken_Drawing.cpp
+++ chicken_Drawing.cpp
@@ -2,44 +2,47 @@
 #include<iostream>
 using namespace std;
 
-void y1();
-void b2();
-void p3();
-void o4();
-void l5();
-void g6();
-
+void y1();  //YELLOW
+void b2();  //BLACK
+void p3();  //PINK
+void o4();  //ORANGE
+void l5();  //SKIN
+void g6();  //LIGHT GREEN Background
 void chicken();
+
+
 main(){
     
     chicken();
     return 0;
 }
 
+
 void y1() {
-    // colour the cell  FFF200 and clear the system from the colour after filling the cell
+    // colour the Terminal-cell with [FFF200][Bright-YELLOW] and  clear the system from the colour after filling the cell.
     cout << "\x1B[48;2;255;255;0m" ; cout << "  " ;
     cout << "\x1B[m" ; }
 
-void b2(){// 000000
+void b2(){// [000000]  BLACK          For the EYES of chicken
     cout << "\x1B[48;2;0;0;0m" ; cout << "  " ;
     cout << "\x1B[m" ; }
 
-void p3(){// ffa3b1
+void p3(){// ffa3b1 PINK              For the CHEEKS of chicken
     cout << "\x1B[48;2;255;163;177m" ; cout << "  " ;
     cout << "\x1B[m" ; }
 
-void o4(){// ff7e00
+void o4(){// ff7e00 ORANGE            For the BEAK of chicken
     cout << "\x1B[48;2;255;126;0m" ; cout << "  " ;
     cout << "\x1B[m" ; }
 
-void l5(){// e5aa7a
+void l5(){// e5aa7a  SKIN COLOUR       For the LEGS of chicken
     cout << "\x1B[48;2;229;170;122m" ; cout << "  " ;
     cout << "\x1B[m" ; }
 
-void g6(){// d3f9bc
+void g6(){// d3f9bc  LIGHT GREEN       For the BACKGROUNF OF IMAGE
     cout << "\x1B[48;2;211;249;188m" ; cout << "  " ;
     cout << "\x1B[m" ; }
+
 
 
 void chicken(){
@@ -48,21 +51,22 @@ void chicken(){
     cout << "\x1B[48;2;255;255;255m" ; cout << "\x1B[38;2;255;0;0m" ;cout << "CALLING CHICKEN BABY...\n" ;
     cout << "\x1B[m" ; }
 
-
-
-    {        g6(); g6(); 
+    {       // row 1
+             g6(); g6(); 
              g6(); g6();
              g6(); g6();
              g6(); g6();
              g6(); g6(); } cout << "\n";  
 
-    {        g6(); g6(); 
+    {        // row 2
+             g6(); g6(); 
              g6(); y1();
              y1(); y1();
              y1(); g6();
              g6(); g6(); } cout << "\n";
 
-    {        g6(); g6(); 
+    {        // row 3
+             g6(); g6(); 
              y1(); y1();
              y1(); y1();
              y1(); y1();
@@ -74,19 +78,22 @@ void chicken(){
              b2(); y1();
              g6(); g6(); } cout << "\n";
 
-    {        g6(); y1(); 
+    {        // row 5
+             g6(); y1(); 
              p3(); p3();
              o4(); o4();
              p3(); p3();
              y1(); g6(); } cout << "\n";
 
-    { g6(); y1();
+    { // row 6
+      g6(); y1();
       y1(); y1();
       y1(); y1();
       y1(); y1();
       y1(); g6(); } cout << "\n";
 
-    { g6(); g6(); 
+    { // row 7
+      g6(); g6(); 
       y1(); y1();
       y1(); y1();
       y1(); y1();
@@ -96,18 +103,19 @@ void chicken(){
       g6(); l5();
       g6(); g6();
       l5(); g6();
-      g6(); g6();} cout << "\n";
+      g6(); g6(); } cout << "\n";
 
     { g6(); g6();
       g6(); g6();
       g6(); g6();
       g6(); g6();
-      g6(); g6();} cout << "\n";
+      g6(); g6(); } cout << "\n";
 
-    { g6(); g6();
+    { // row 10th
       g6(); g6();
       g6(); g6();
       g6(); g6();
-      g6(); g6();} cout << "\n";
+      g6(); g6();
+      g6(); g6(); } cout << "\n";
 
 }
